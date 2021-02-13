9d772c2015f6920fcf637b3f3febcb882eb884f2
Pocol <project.asura@gmail.com>
2021-02-13 23:22:12 +0900

    ベースコード追加。

---
+++ D3D12_LocalRootSig/src/SampleApp.cpp
@@ -0,0 +1,310 @@
+﻿//-----------------------------------------------------------------------------
+// File : SampleApp.cpp
+// Desc : Sample Application.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <SampleApp.h>
+#include <asdxLogger.h>
+#include <asdxCmdHelper.h>
+
+
+namespace {
+
+///////////////////////////////////////////////////////////////////////////////
+// SceneParam structure
+///////////////////////////////////////////////////////////////////////////////
+struct alignas(256) SceneParam
+{
+    asdx::Matrix    InvView;
+    asdx::Matrix    InvViewProj;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Vertex structure
+///////////////////////////////////////////////////////////////////////////////
+struct Vertex
+{
+    asdx::Vector3   Position;
+    asdx::Vector4   Color;
+};
+
+} // namespace
+
+
+///////////////////////////////////////////////////////////////////////////////
+// SampleApp class
+///////////////////////////////////////////////////////////////////////////////
+
+//-----------------------------------------------------------------------------
+//      コンストラクタです.
+//-----------------------------------------------------------------------------
+SampleApp::SampleApp()
+: asdx::Application(L"Sample", 960, 540, nullptr, nullptr, nullptr)
+{
+    m_SwapChainFormat = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+}
+
+//-----------------------------------------------------------------------------
+//      デストラクタです.
+//-----------------------------------------------------------------------------
+SampleApp::~SampleApp()
+{
+}
+
+//-----------------------------------------------------------------------------
+//      初期化処理です.
+//-----------------------------------------------------------------------------
+bool SampleApp::OnInit()
+{
+    auto pDevice = asdx::GfxDevice().GetDevice();
+    m_pGraphicsQueue = asdx::GfxDevice().GetGraphicsQueue();
+
+    // DXRがサポートされているかどうかチェック.
+    if (!asdx::IsSupportDXR(pDevice))
+    {
+        ELOGA("Error : DXR Unsupported.");
+        return false;
+    }
+
+    // コンピュートターゲット生成.
+    {
+        asdx::TargetDesc desc;
+        desc.Dimension          = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+        desc.Width              = m_Width;
+        desc.Height             = m_Height;
+        desc.DepthOrArraySize   = 1;
+        desc.Format             = DXGI_FORMAT_R8G8B8A8_UNORM;
+        desc.MipLevels          = 1;
+        desc.SampleDesc.Count   = 1;
+        desc.SampleDesc.Quality = 0;
+        desc.InitState          = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+
+        if (!m_Canvas.Init(&desc))
+        {
+            ELOGA("Error : ComputeTarget::Init() Failed.");
+            return false;
+        }
+    }
+
+    // シーンバッファ初期化.
+    {
+        if (!m_SceneCB.Init(sizeof(SceneParam)))
+        {
+            ELOGA("Error : ConstantBuffer::Init() Failed.");
+            return false;
+        }
+    }
+
+    // 頂点データ生成.
+    {
+        const Vertex vertices[] = {
+            { asdx::Vector3( 0.0f,  0.7f, 1.0f), asdx::Vector4(1.0f, 0.0f, 0.0f, 1.0f) },
+            { asdx::Vector3(-0.7f, -0.7f, 1.0f), asdx::Vector4(0.0f, 1.0f, 0.0f, 1.0f) },
+            { asdx::Vector3( 0.7f, -0.7f, 1.0f), asdx::Vector4(0.0f, 0.0f, 1.0f, 1.0f) },
+        };
+
+        auto size = sizeof(vertices);
+        if (!asdx::CreateUploadBuffer(pDevice, size, m_VB.GetAddress()))
+        {
+            ELOGA("Error : CreateUploadBuffer() Failed.");
+            return false;
+        }
+
+        void* ptr = nullptr;
+        auto hr = m_VB->Map(0, nullptr, &ptr);
+        if (FAILED(hr))
+        {
+            ELOGA("Error : ID3D12Resource::Map() Failed. errcode = 0x%x", hr);
+            return false;
+        }
+
+        memcpy(ptr, vertices, sizeof(vertices));
+        m_VB->Unmap(0, nullptr);
+
+        if (!asdx::CreateBufferSRV(pDevice, m_VB.GetPtr(), sizeof(vertices) / 4, 0, m_VertexSRV.GetAddress()))
+        {
+            ELOGA("Error : CreateBufferSRV() Failed.");
+            return false;
+        }
+    }
+
+    // インデックスデータ生成.
+    {
+        uint32_t indices[] = { 0, 1, 2 };
+
+        if (!asdx::CreateUploadBuffer(pDevice, sizeof(indices), m_IB.GetAddress()))
+        {
+            ELOGA("Error : CreateUploadBuffer() Failed.");
+            return false;
+        }
+
+        void* ptr = nullptr;
+        auto hr = m_IB->Map(0, nullptr, &ptr);
+        if (FAILED(hr))
+        {
+            ELOGA("Error : ID3D12Resource::Map() Failed. errcode = 0x%x", hr);
+            return false;
+        }
+
+        memcpy(ptr, indices, sizeof(indices));
+        m_IB->Unmap(0, nullptr);
+
+        if (!asdx::CreateBufferSRV(pDevice, m_IB.GetPtr(), 3, 0, m_IndexSRV.GetAddress()))
+        {
+            ELOGA("Error : CreateBufferSRV() Failed.");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+//      終了処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnTerm()
+{
+    m_GlobalRootSig .Reset();
+    m_LocalRootSig  .Reset();
+    m_StateObject   .Reset();
+    m_VB            .Reset();
+    m_IB            .Reset();
+    m_TLAS          .Term();
+    m_BLAS          .Term();
+    m_Canvas        .Term();
+    m_BackGround    .Term();
+    m_RayGenTable   .Term();
+    m_MissTable     .Term();
+    m_HitGroupTable .Term();
+    m_SceneCB       .Term();
+    m_VertexSRV     .Reset();
+    m_IndexSRV      .Reset();
+}
+
+//-----------------------------------------------------------------------------
+//      フレーム描画処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnFrameRender(asdx::FrameEventArgs& args)
+{
+    if (m_pGraphicsQueue == nullptr)
+    { return; }
+
+    auto idx  = GetCurrentBackBufferIndex();
+    auto pCmd = m_GfxCmdList.Reset();
+
+    asdx::GfxDevice().SetUploadCommand(pCmd);
+
+    // シーンバッファ更新.
+    {
+        // ビュー行列.
+        auto view = asdx::Matrix::CreateLookAt(
+            asdx::Vector3(0.0f, 0.0f, -2.0f),
+            asdx::Vector3(0.0f, 0.0f, 0.0f),
+            asdx::Vector3(0.0f, 1.0f, 0.0f));
+
+        // 射影行列.
+        auto proj = asdx::Matrix::CreatePerspectiveFieldOfView(
+            asdx::ToRadian(37.5f),
+            float(m_Width) / float(m_Height),
+            1.0f,
+            1000.0f);
+
+        SceneParam res = {};
+        res.InvView     = asdx::Matrix::Invert(view);
+        res.InvViewProj = asdx::Matrix::Invert(proj) * res.InvView;
+
+        m_SceneCB.SwapBuffer();
+        m_SceneCB.Update(&res, sizeof(res));
+    }
+
+    // レイトレ実行.
+    {
+        //// グローバルルートシグニチャ設定.
+        //pCmd->SetComputeRootSignature(m_GlobalRootSig.GetPtr());
+
+        //// ステートオブジェクト設定.
+        //pCmd->SetPipelineState1(m_StateObject.GetPtr());
+
+        //// リソースをバインド.
+        //asdx::SetTable(pCmd, ROOT_PARAM_U0, m_Canvas      .GetUAV(), true);
+        //asdx::SetSRV  (pCmd, ROOT_PARAM_T0, m_TLAS   .GetResource(), true);
+        //asdx::SetTable(pCmd, ROOT_PARAM_T1, m_BackGround .GetView(), true);
+        //asdx::SetSRV  (pCmd, ROOT_PARAM_T2, m_IndexSRV   .GetPtr (), true);
+        //asdx::SetSRV  (pCmd, ROOT_PARAM_T3, m_VertexSRV  .GetPtr (), true);
+        //asdx::SetCBV  (pCmd, ROOT_PARAM_B0, m_SceneBuffer.GetView(), true);
+
+        //D3D12_DISPATCH_RAYS_DESC desc = {};
+        //desc.HitGroupTable              = m_HitGroupTable.GetTableView();
+        //desc.MissShaderTable            = m_MissTable    .GetTableView();
+        //desc.RayGenerationShaderRecord  = m_RayGenTable  .GetRecordView();
+        //desc.Width                      = UINT(m_Canvas.GetDesc().Width);
+        //desc.Height                     = m_Canvas.GetDesc().Height;
+        //desc.Depth                      = 1;
+
+        //pCmd->DispatchRays(&desc);
+
+        //// バリアを張っておく.
+        //asdx::BarrierUAV(pCmd, m_Canvas.GetResource());
+    }
+
+    // レイトレ結果をバックバッファにコピー.
+    {
+        asdx::ScopedTransition b0(pCmd, m_Canvas.GetResource(), asdx::STATE_UAV, asdx::STATE_COPY_SRC);
+        asdx::ScopedTransition b1(pCmd, m_ColorTarget[idx].GetResource(), asdx::STATE_PRESENT, asdx::STATE_COPY_DST);
+        pCmd->CopyResource(m_ColorTarget[idx].GetResource(), m_Canvas.GetResource());
+    }
+
+    pCmd->Close();
+
+    ID3D12CommandList* pCmds[] = {
+        pCmd
+    };
+
+    // 前フレームの描画の完了を待機.
+    if (m_FrameWaitPoint.IsValid())
+    { m_pGraphicsQueue->Sync(m_FrameWaitPoint); }
+
+    // コマンドを実行.
+    m_pGraphicsQueue->Execute(1, pCmds);
+
+    // 待機点を発行.
+    m_FrameWaitPoint = m_pGraphicsQueue->Signal();
+
+    // 画面に表示.
+    Present(0);
+
+    // フレーム同期.
+    asdx::GfxDevice().FrameSync();
+}
+
+//-----------------------------------------------------------------------------
+//      リサイズ処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnResize(const asdx::ResizeEventArgs& args)
+{
+}
+
+//-----------------------------------------------------------------------------
+//      キー処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnKey(const asdx::KeyEventArgs& args)
+{
+}
+
+//-----------------------------------------------------------------------------
+//      マウス処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnMouse(const asdx::MouseEventArgs& args)
+{
+}
+
+//-----------------------------------------------------------------------------
+//      タイピング処理です.
+//-----------------------------------------------------------------------------
+void SampleApp::OnTyping(uint32_t keyCode)
+{
+}
