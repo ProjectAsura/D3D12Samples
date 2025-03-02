ab732faf757111e48f18faf606741d6603eb792c
Pocol <project.asura@gmail.com>
2025-03-02 15:36:24 +0900

    コミット漏れ対応.

---
+++ D3D12_DRED/PageFaultSample/include/DredReport.h
@@ -0,0 +1,10 @@
+﻿//-----------------------------------------------------------------------------
+// File : DredReport.h
+// Desc : Device Removed Extended Data Reporter.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+#pragma once
+
+#include <d3d12.h>
+
+void ReportDRED(HRESULT hr, ID3D12Device* pDevice);
